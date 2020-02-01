d91b7d89f640e8787802d1ed0b6a9cdad36c8e85
higormonteiro <higor.tecinfor@gmail.com>
2020-02-01 19:59:01 -0300

    create model project

--- todolist/models.py
+++ todolist/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Assignment(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    project_by = models.ForeignKey(Project, on_delete=models.CASCADE)
