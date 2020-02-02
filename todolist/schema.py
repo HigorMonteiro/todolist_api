5b85b77d80287134c3fa631b9f65087e50762e1a
higormonteiro <higor.tecinfor@gmail.com>
2020-02-02 11:07:32 -0300

    add Query and Mutation to model Project

--- todolist/schema.py
+++ todolist/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Assignment
+from .models import Assignment, Project
 
 
 class AssignmentType(DjangoObjectType):
@@ -9,24 +9,55 @@ class AssignmentType(DjangoObjectType):
         model = Assignment
 
 
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+
+
 class Query(graphene.ObjectType):
     assignments = graphene.List(AssignmentType)
+    project = graphene.List(ProjectType)
 
     def resolve_assignments(self, info):
         return Assignment.objects.all()
 
+    def resolve_project(self, info):
+        return Project.objects.all()
+
 
 class CreateAssignment(graphene.Mutation):
     assignment = graphene.Field(AssignmentType)
+    project = graphene.Field(ProjectType)
 
     class Arguments:
         title = graphene.String()
         description = graphene.String()
+        project_by = graphene.Int(required=True)
 
-    def mutate(self, info, title, description):
-        assignment = Assignment(title=title, description=description)
+    def mutate(self, info, title, description, project_by):
+        project = Project.objects.get(id=project_by)
+
+        if not project:
+            raise Exception('Cannot find project')
+
+        assignment = Assignment(
+            title=title,
+            description=description,
+            project_by=project)
         assignment.save()
         return CreateAssignment(assignment=assignment)
+
+
+class CreateProject(graphene.Mutation):
+    project = graphene.Field(ProjectType)
+
+    class Arguments:
+        name = graphene.String()
+
+    def mutate(self, info, name):
+        project = Project(name=name)
+        project.save()
+        return CreateProject(project=project)
 
 
 class UpdateAssignment(graphene.Mutation):
@@ -36,12 +67,14 @@ class UpdateAssignment(graphene.Mutation):
         assignment_id = graphene.Int(required=True)
         title = graphene.String()
         description = graphene.String()
+        project_by = graphene.Int(required=True)
 
-    def mutate(self, info, assignment_id, title, description):
+    def mutate(self, info, assignment_id, title, description, project_by):
         assignment = Assignment.objects.get(id=assignment_id)
 
         assignment.title = title
         assignment.description = description
+        assignment.project_by = assignment
         assignment.save()
 
         return UpdateAssignment(assignment=assignment)
@@ -64,3 +97,4 @@ class Mutation(graphene.ObjectType):
     create_assignment = CreateAssignment.Field()
     update_assignment = UpdateAssignment.Field()
     delete_assignment = DeleteAssignment.Field()
+    create_project = CreateProject.Field()
